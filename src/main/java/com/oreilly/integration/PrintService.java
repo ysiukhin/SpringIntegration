8c3679184ebea5564181ccf7a107e39321c64eb9
ysiukhin <wBqJ2eNs>
2022-04-17 22:15:36 +0300

    Message step#1

---
+++ src/main/java/com/oreilly/integration/PrintService.java
@@ -0,0 +1,9 @@
+package com.oreilly.integration;
+
+import org.springframework.messaging.Message;
+
+public class PrintService {
+    public void print(Message<String> message) {
+        System.out.println(message.getPayload());
+    }
+}
--- src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
+++ src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
 
 @SpringBootApplication
 @Configuration
@@ -18,6 +20,8 @@ public class SpringIntegrationApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Hello World.");
+        Message<String> message = new GenericMessage<>("Hello World");
+        PrintService service = new PrintService();
+        service.print(message);
     }
 }
