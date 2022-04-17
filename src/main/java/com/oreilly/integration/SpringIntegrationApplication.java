8d5c363a028c33104c71d214a3d5424c3e8e8ce1
ysiukhin <wBqJ2eNs>
2022-04-17 22:44:22 +0300

    Channel: using MessageHandler()

--- src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
+++ src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -1,13 +1,17 @@
 package com.oreilly.integration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
 
@@ -19,6 +23,9 @@ import java.util.Map;
 @ImportResource("integration-context.xml")
 public class SpringIntegrationApplication implements ApplicationRunner {
 
+    @Autowired
+    private DirectChannel channel;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringIntegrationApplication.class, args);
     }
@@ -26,11 +33,17 @@ public class SpringIntegrationApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
+        channel.subscribe(new MessageHandler() {
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                new PrintService().print((Message<String>) message);
+            }
+        });
+
         Message<String> message = MessageBuilder
                 .withPayload("Hello World, from the builder pattern")
                 .setHeader("newHeader", "newHeaderValue")
                 .build();
-        PrintService service = new PrintService();
-        service.print(message);
+        channel.send(message);
     }
 }
