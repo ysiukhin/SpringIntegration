6a60ea61c68cbe3dff57b5b6fb8a6a8f7fc5c0c1
ysiukhin <wBqJ2eNs>
2022-04-22 21:12:48 +0300

    0405.Channels: using DirectChannel with Round robin load balancer (when an exception is been throw on one of subscribed service activators and  <int:dispatcher failover="true" /> then the message will be resend to other service activator)

--- src/main/java/com/oreilly/integration/PrintService.java
+++ src/main/java/com/oreilly/integration/PrintService.java
@@ -4,6 +4,7 @@ import org.springframework.messaging.Message;
 
 public class PrintService {
     public void print(Message<String> message) {
-        System.out.println(message.getPayload());
+        throw new RuntimeException("PrintService throws an exception.");
+        //        System.out.println(message.getPayload());
     }
 }
