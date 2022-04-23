f246e0d75ea5da8fff9664766d4e1bebd2b20b43
ysiukhin <wBqJ2eNs>
2022-04-23 21:27:53 +0300

    0507.Routing Messages: <int:splitter> custom split method.

--- src/main/java/com/oreilly/integration/CustomSplitter.java
+++ src/main/java/com/oreilly/integration/CustomSplitter.java
@@ -1,14 +1,13 @@
 package com.oreilly.integration;
 
-import org.springframework.integration.splitter.AbstractMessageSplitter;
 import org.springframework.messaging.Message;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class CustomSplitter extends AbstractMessageSplitter {
-    @Override
-    protected Object splitMessage(Message<?> message) {
+public class CustomSplitter {
+
+    public List<String> split(Message<?> message) {
         return Arrays.asList(message.getPayload().toString().split(" "));
     }
 }
