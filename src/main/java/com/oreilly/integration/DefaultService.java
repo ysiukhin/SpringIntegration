77c9b4ca93e9eb0d6ed8d20c011540501756ba13
ysiukhin <wBqJ2eNs>
2022-04-24 08:29:30 +0300

    0602.Transforming Messages: <int:transformer> using SpEL

--- src/main/java/com/oreilly/integration/DefaultService.java
+++
@@ -1,9 +0,0 @@
-package com.oreilly.integration;
-
-import org.springframework.messaging.Message;
-
-public class DefaultService {
-    public void print(Message<?> message) {
-            System.out.println("Printing from the default service");
-    }
-}
