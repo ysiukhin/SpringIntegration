36ff4b4ec47ca50c3cff8fbb1e350bad497e63fb
ysiukhin <wBqJ2eNs>
2022-04-25 21:00:05 +0300

    0802.File System Integration:  <int-file:inbound-channel-adapter> PS. You need to explicitly add dependency:         <dependency>             <groupId>org.springframework.integration</groupId>             <artifactId>spring-integration-file</artifactId>         </dependency>
    Spring boot(1.4.X) onwards below modules has been removed:
            spring-integration-file
            spring-integration-http
            spring-integration-ip
            spring-integration-stream
    https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-1.4-Release-Notes

---
+++ src/main/java/com/oreilly/integration/FilePrinter.java
@@ -0,0 +1,25 @@
+package com.oreilly.integration;
+
+import java.io.*;
+
+public class FilePrinter {
+    public void print(File file) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
