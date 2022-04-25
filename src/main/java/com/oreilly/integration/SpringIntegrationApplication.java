96d4c0fac5db21558914f7031ed38a0bd3da2a76
ysiukhin <wBqJ2eNs>
2022-04-25 21:27:39 +0300

    0803.File System Integration:  <int-file:outbound-channel-adapter> PS. You need to explicitly add dependency:         <dependency>             <groupId>org.springframework.integration</groupId>             <artifactId>spring-integration-file</artifactId>         </dependency>
    Spring boot(1.4.X) onwards below modules has been removed:
            spring-integration-file
            spring-integration-http
            spring-integration-ip
            spring-integration-stream
    https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-1.4-Release-Notes

--- src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
+++ src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -1,21 +1,11 @@
 package com.oreilly.integration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.messaging.Message;
-import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.support.GenericMessage;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
-import sun.jvm.hotspot.runtime.Thread;
-
-import java.time.temporal.ChronoUnit;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @Configuration
