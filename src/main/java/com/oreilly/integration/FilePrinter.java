2eb124b4ba57aa17e95ff92cbeb4c062c011f0ba
ysiukhin <wBqJ2eNs>
2022-04-25 21:39:35 +0300

    0804.File System Integration:  <int-file:file-to-string-transformer>
    PS. You need to explicitly add dependency:
            <dependency>
                <groupId>org.springframework.integration</groupId>
                <artifactId>spring-integration-file</artifactId>
            </dependency>
    
    Spring boot(1.4.X) onwards below modules has been removed:
            spring-integration-file
            spring-integration-http
            spring-integration-ip
            spring-integration-stream
    https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-1.4-Release-Notes

--- src/main/java/com/oreilly/integration/FilePrinter.java
+++ src/main/java/com/oreilly/integration/FilePrinter.java
@@ -3,6 +3,11 @@ package com.oreilly.integration;
 import java.io.*;
 
 public class FilePrinter {
+    public void print(String file) {
+        System.out.println("Invoking the print method with a string");
+        System.out.println(file);
+    }
+
     public void print(File file) {
         BufferedReader reader = null;
         try {
