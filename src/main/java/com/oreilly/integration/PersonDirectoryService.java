8bc993d36db6b655bb3d80ccb7687d23888de7c3
ysiukhin <wBqJ2eNs>
2022-04-25 17:01:18 +0300

    0707.Processing Endpoints:  <int:outbound-channel-adapter>

--- src/main/java/com/oreilly/integration/PersonDirectoryService.java
+++ src/main/java/com/oreilly/integration/PersonDirectoryService.java
@@ -2,6 +2,7 @@ package com.oreilly.integration;
 
 public class PersonDirectoryService {
     public Person findNewPeople() {
+        System.out.println("Inbound Adapter has polled this service.");
         return new Person("Jane", "Doe");
     }
 }
