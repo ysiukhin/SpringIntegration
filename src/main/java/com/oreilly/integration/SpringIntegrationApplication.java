6bacc924fa20d628a003f47ffae20a2658664110
ysiukhin <wBqJ2eNs>
2022-04-26 23:21:59 +0300

    1005.JDBC Integration:  <int-jdbc:outbound-gateway>
    PS. You need to explicitly add dependencies.

--- src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
+++ src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@Configuration
-@ImportResource("integration-context.xml")
 public class SpringIntegrationApplication implements ApplicationRunner {
 
     @Autowired
---
+++ src/main/java/com/oreilly/integration/WebConfiguration.java
@@ -0,0 +1,20 @@
+package com.oreilly.integration;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+//Configuration for the H2 In Memory Database Console
+
+@Configuration
+@ImportResource("integration-context.xml")
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
